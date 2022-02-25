e8930482d153ab48d5032e8cb10cde095ff640d2
Preeti Jatav <preeti.jatav@Preeti-Jatav-MBP.local>
2022-02-25 11:49:47 +0530

    first commit

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Card(){
+    return (
+      <div>
+      <h1> This is the right side</h1>
+      <hr />
+      <p>
+       fdgfgfhfghgf fghfghgfhgfhgf  gfhgfh
+      </p>
+         
+      </div>
+    );
+  }
+export default Card;
